24f56c073c54a6f125316363621577b96566b10e
JeanDevFR <42603119+JeanDevFR@users.noreply.github.com>
2021-10-29 07:01:15 +0200

    Add SQLAlchemy ORM

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
+from os import SEEK_SET
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_pyfile('settings.py')
+
+db = SQLAlchemy(app)
 
 import routes
