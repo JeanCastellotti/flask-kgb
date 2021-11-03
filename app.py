b4871352dcaccab43c1415237e90971b2f194497
JeanDevFR <42603119+JeanDevFR@users.noreply.github.com>
2021-11-03 08:17:52 +0100

    Add flask-login

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_assets import Environment, Bundle
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Application
 app = Flask(__name__)
 app.config.from_pyfile('settings.py')
+
+# Bcrypt
+bcrypt = Bcrypt(app)
+
+# Login
+login_manager = LoginManager(app)
+login_manager.login_view = 'auth.login'
+login_manager.login_message = 'Veuillez vous connecter pour accéder à cette page.'
+login_manager.login_message_category = 'warning'
 
 # Database ORM
 db = SQLAlchemy(app)
