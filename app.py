3fc8faf3e0ca768c421344d61b5deaec837b1f7b
JeanDevFR <42603119+JeanDevFR@users.noreply.github.com>
2021-11-03 08:13:34 +0100

    Add Stylus

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from os import SEEK_SET
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_assets import Environment, Bundle
 
+# Application
 app = Flask(__name__)
 app.config.from_pyfile('settings.py')
 
+# Database ORM
 db = SQLAlchemy(app)
 
-import routes
+# Stylus
+assets = Environment(app)
+stylus = Bundle('css/src/main.styl',
+                depends='css/src/partials/**.styl',
+                filters='stylus,autoprefixer6,cssmin',
+                output='css/style.css')
+assets.register('stylus', stylus)
