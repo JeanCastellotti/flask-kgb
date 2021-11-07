cfb04b60c1c2dd37872398fa015c5c0c3300e2f6
Jean <castellotti.j@gmail.com>
2021-11-07 13:00:08 +0100

    Add index pagination

--- main/routes.py
+++ main/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, request
 from models import Mission
 
 main = Blueprint('main', __name__)
@@ -6,12 +6,13 @@ main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    missions = Mission.query.all()
+    page = request.args.get('page', 1, type=int)
+    missions = Mission.query.order_by(Mission.id.desc()).paginate(page=page,
+                                                                  per_page=5)
     return render_template('index.html', missions=missions)
 
 
 @main.route('/mission/<int:id>')
 def mission(id):
     mission = Mission.query.get_or_404(id)
-    print(mission.agents)
     return render_template('mission.html', mission=mission)
