650eabcf08c48e2aea966df64e3b9fcbb131f9fc
Jean <castellotti.j@gmail.com>
2021-11-07 09:06:04 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -3,4 +3,5 @@ import os
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace(
     'postgres://', 'postgresql://')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 SECRET_KEY = os.environ.get('SECRET_KEY')
