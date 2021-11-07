4b2659753591aa82339aa45bc3dc81ba7a2a6ded
Jean <castellotti.j@gmail.com>
2021-11-07 12:59:29 +0100

    Add missions pagination

--- admin/routes.py
+++ admin/routes.py
@@ -10,7 +10,9 @@ admin = Blueprint('admin', __name__, url_prefix='/admin')
 @admin.route('/missions')
 @login_required
 def missions():
-    missions = Mission.query.order_by(Mission.id.desc()).all()
+    page = request.args.get('page', 1, type=int)
+    missions = Mission.query.order_by(Mission.id.desc()).paginate(page=page,
+                                                                  per_page=5)
     return render_template('admin/missions.html', missions=missions)
 
 
